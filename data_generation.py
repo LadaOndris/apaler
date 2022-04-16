Fixed several issues, including computation of angles

data_generation.py
@@ -1,5 +1,3 @@
-import math
-
 import cv2
 import numpy as np
 
@@ -15,24 +13,42 @@ def get_cameras():
     # Lens: Kowa LM50-IR-F
     # Focal length: 50 mm?
     pixel_size = 3.45 * 1e-6
-    focal_length = 10 * 1e-3
+    focal_length = 20 * 1e-3
+    elevation = 30
     cam1 = Camera(Position(2000, 0, 500),
                   ImageSize(6480, 4860),
-                  CameraOrientation(90 - 75.07, 1.5),
+                  CameraOrientation(90 - 75.07, elevation),
                   focal_length, pixel_size)
 
     cam2 = Camera(Position(0, 1500, 500),
                   ImageSize(6480, 4860),
-                  CameraOrientation(90 - 65.61, 1.5),
+                  CameraOrientation(90 - 65.61, elevation),
                   focal_length, pixel_size)
     # print(F"Distance betwen cameras: {get_distance(cam1.position, cam2.position)}")
     return cam1, cam2
 
 
-def get_distance(pos1: Position, pos2: Position):
-    return math.sqrt(math.pow(pos1.x - pos2.x, 2) +
-                     math.pow(pos1.y - pos2.y, 2) +
-                     math.pow(pos1.z - pos2.z, 2))
+# def get_cameras():
+#     # Camera: Prosilica GT 6400
+#     # https://www.alliedvision.com/en/camera-selector/detail/prosilica-gt/6400/
+#     # Pixel size	3.45 µm × 3.45 µm
+#     # Resolution: 6480 (H) × 4860 (V)
+#     # Lens: Kowa LM50-IR-F
+#     # Focal length: 50 mm?
+#     pixel_size = 3.45 * 1e-6
+#     focal_length = 20 * 1e-3
+#     elevation = 0
+#     cam1 = Camera(Position(1243, 0, 500),
+#                   ImageSize(6480, 4860),
+#                   CameraOrientation(0, elevation),
+#                   focal_length, pixel_size)
+#
+#     cam2 = Camera(Position(3000, 0, 500),
+#                   ImageSize(6480, 4860),
+#                   CameraOrientation(-45, elevation),
+#                   focal_length, pixel_size)
+#     # print(F"Distance betwen cameras: {get_distance(cam1.position, cam2.position)}")
+#     return cam1, cam2
 
 
 def test_camera_image(cam):
@@ -40,30 +56,43 @@ def test_camera_image(cam):
     cam.display_image(image)
 
 
+def get_transformation_matrix(translation_vec: np.ndarray, azim_rads: float, elev_rads: float):
+    translation_matrix = np.array([[1, 0, 0, translation_vec[0]],
+                                   [0, 1, 0, translation_vec[1]],
+                                   [0, 0, 1, translation_vec[2]],
+                                   [0, 0, 0, 1]])
+    # Rotate line into the camera's view (Y axis)
+    # counterclockwise
+    rotation_matrix_azim = np.array([[np.cos(azim_rads), -np.sin(azim_rads), 0, 0],
+                                     [np.sin(azim_rads), np.cos(azim_rads), 0, 0],
+                                     [0, 0, 1, 0],
+                                     [0, 0, 0, 1]])
+    # clockwise
+    rotation_matrix_elev = np.array([[1, 0, 0, 0],
+                                     [0, np.cos(elev_rads), np.sin(elev_rads), 0],
+                                     [0, -np.sin(elev_rads), np.cos(elev_rads), 0],
+                                     [0, 0, 0, 1]])
+    # The order of matrix multiplication matters!
+    projection_matrix = rotation_matrix_elev @ rotation_matrix_azim @ translation_matrix
+    return projection_matrix
+
+
 def project_line(cam: Camera, line: Line):
     image = np.zeros((cam.resolution.height, cam.resolution.width))
     # Translate camera's and  positions  into the origin
     # X and Z are width and height, Y is distance to the line
-    translation = -cam.position.to_array()
-    p1_trans = line.pos1.to_array() + translation
-    p2_trans = line.pos2.to_array() + translation
 
     # Angles to radians
     azim_rads = cam.orientation.azimuth / 180 * np.pi
     elev_rads = cam.orientation.elevation / 180 * np.pi
+    translation = -cam.position.to_array()
+    transformation_matrix = get_transformation_matrix(translation, azim_rads, elev_rads)
 
-    # Rotate line into the camera's view (Y axis)
-    # counterclockwise
-    rotation_matrix_azim = np.array([[np.cos(azim_rads), -np.sin(azim_rads), 0],
-                                     [np.sin(azim_rads), np.cos(azim_rads), 0],
-                                     [0, 0, 1]])
-    # clockwise
-    rotation_matrix_elev = np.array([[1, 0, 0],
-                                     [0, np.cos(elev_rads), np.sin(elev_rads)],
-                                     [0, -np.sin(elev_rads), np.cos(elev_rads)]])
-    rotation_matrix = np.dot(rotation_matrix_azim, rotation_matrix_elev)
-    p1 = np.dot(rotation_matrix, p1_trans)
-    p2 = np.dot(rotation_matrix, p2_trans)
+    p1_orig = np.concatenate([line.pos1.to_array(), [1]], axis=-1)
+    p2_orig = np.concatenate([line.pos2.to_array(), [1]], axis=-1)
+
+    p1 = np.dot(transformation_matrix, p1_orig)
+    p2 = np.dot(transformation_matrix, p2_orig)
 
     # Rasterize line from p1 to p2
     points = rasterize_line_3d(Position.from_array(p1),
@@ -106,10 +135,13 @@ def project_and_display(cam: Camera, line: Line, show: bool, save: str = None):
 if __name__ == "__main__":
     cam1, cam2 = get_cameras()
     # test_camera_image(cam1)
-    # line = Line(Position(2500, 6600, 500), Position(1000, 9000, 10000))
+    line = Line(Position(2800, 6600, 500), Position(1000, 9000, 10000))  # Line at an angle
+    # line = Line(Position(2800, 6600, 500), Position(2800, 6600, 10000))  # Vertical line
+    # line = Line(Position(1600, 3200, 500), Position(1600, 3200, 10000))  # Vertical line a bit closer
+    # line = Line(Position(0, 3000, 500), Position(-10000, 3000, 3500))
     # line = Line(Position(2400, 4600, 500), Position(1000, 7000, 10000))
     # line = Line(Position(4000, 11000, 500), Position(4000, 7000, 10000))
-    line = Line(Position(2000, 3000, 500), Position(1000, 4000, 10000))
-    # line = Line(Position(3000, 1000, 500), Position(1000, 1000, 500))
+    # line = Line(Position(2000, 3000, 500), Position(1000, 4000, 10000))
+    # line = Line(Position(3000, 6000, 500), Position(1000, 6000, 500))
     project_and_display(cam1, line, show=True, save='./data/line_0_0.png')
     project_and_display(cam2, line, show=True, save='./data/line_0_1.png')

data_types.py
@@ -1,5 +1,6 @@
 import math
 from typing import Tuple
+from geometry import get_distance
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -63,23 +64,55 @@ class Camera:
         self.orientation = orientation
         self.focal_length = focal_length
         self.pixel_size = pixel_size
-        # self.ifov_azim = 2 * math.atan(pixel_size * resolution.width / (2 * focal_length)) / np.pi * 180 / resolution.width
-        # self.ifov_elev = 2 * math.atan(pixel_size * resolution.height / (2 * focal_length)) / np.pi * 180 / resolution.height
-        self.ifov_azim = 2 * math.atan(
-            pixel_size / (2 * focal_length)) / np.pi * 180
-        self.ifov_elev = 2 * math.atan(
-            pixel_size / (2 * focal_length)) / np.pi * 180
+
+        self.fov_azim = 2 * math.atan(pixel_size * resolution.width / (2 * focal_length)) / np.pi * 180
+        self.fov_elev = 2 * math.atan(pixel_size * resolution.height / (2 * focal_length)) / np.pi * 180
+
         self.pixel_azimuths, self.pixel_elevations = self.compute_per_pixel_orientation()
 
-    def compute_per_pixel_orientation(self) -> Tuple[np.ndarray, np.ndarray]:
-        ifov_width = self.resolution.width * self.ifov_azim
-        ifov_height = self.resolution.height * self.ifov_elev
-        pixel_azimuths = np.arange(0, ifov_width, self.ifov_azim)
-        pixel_elevations = np.arange(0, ifov_height, self.ifov_elev)
-        pixel_elevations = np.flip(pixel_elevations)
+    def half_ifovs(self, num_pixels: int):
+        # Cumulative sum of pixel sizes
+        pixel_sizes = np.full(shape=[num_pixels], fill_value=self.pixel_size)
+        cum_pixel_sizes = np.cumsum(pixel_sizes)
+        # Compute angles for cumulative sum of pixel sizes
+        cum_ifov = np.arctan(cum_pixel_sizes / self.focal_length)
+        # Undo cumsum
+        ifov_difs_rads = cum_ifov[1:] - cum_ifov[:-1].copy()
+        # Prepend the first element that was forgotten in the previous step
+        ifov_rads = np.concatenate([[cum_ifov[0]], ifov_difs_rads], axis=-1)
+        # Convert to degrees
+        ifov_degs = ifov_rads / np.pi * 180
+        return ifov_degs
 
-        pixel_azimuths += self.orientation.azimuth - ifov_width / 2
-        pixel_elevations += self.orientation.elevation - ifov_height / 2
+    def compute_per_pixel_orientation(self) -> Tuple[np.ndarray, np.ndarray]:
+        # ifov_width = self.resolution.width * self.ifov_azim
+        # ifov_height = self.resolution.height * self.ifov_elev
+        # pixel_azimuths = np.arange(0, ifov_width, self.ifov_azim) + self.ifov_azim
+        # pixel_elevations = np.arange(0, ifov_height, self.ifov_elev) + self.ifov_elev
+        # pixel_elevations = np.flip(pixel_elevations)
+
+        half_ifovs_azim = self.half_ifovs(int(self.resolution.width / 2))
+        half_ifovs_elev = self.half_ifovs(int(self.resolution.height / 2))
+
+        half_ifovs_azim_cum = np.cumsum(half_ifovs_azim)
+        half_ifovs_elev_cum = np.cumsum(half_ifovs_elev)
+
+        # Flip the array symmetrically
+        reversed_half_ifovs_azim = np.flipud(half_ifovs_azim_cum)
+        reversed_half_ifovs_elev = np.flipud(half_ifovs_elev_cum)
+
+        # There is no 0 in the cetre of the image -> add it manually
+        # by removing the first element and later adding 0.
+        reversed_half_ifovs_azim_corrected = reversed_half_ifovs_azim[1:]
+        reversed_half_ifovs_elev_corrected = reversed_half_ifovs_elev[1:]
+
+        # Concatenate symmetrically and add the 0.
+        ifovs_azim = np.concatenate([-reversed_half_ifovs_azim_corrected, [0], half_ifovs_azim_cum])
+        ifovs_elev = np.concatenate([-reversed_half_ifovs_elev_corrected, [0], half_ifovs_elev_cum])
+        ifovs_elev_flipped = np.flip(ifovs_elev)
+
+        pixel_azimuths = ifovs_azim + self.orientation.azimuth
+        pixel_elevations = ifovs_elev_flipped + self.orientation.elevation
         return pixel_azimuths, pixel_elevations
 
     def display_image(self, image: np.ndarray):

detection/hough.py
@@ -3,7 +3,7 @@ from typing import List, Tuple
 import cv2
 import numpy as np
 
-from data_generation import get_cameras
+from data_generation import get_cameras, get_transformation_matrix
 from data_types import Camera, CandidatePlane, ImageCameraPair, Line, Position
 from detection.base import LineDetector
 
@@ -18,31 +18,34 @@ class HoughLineDetector(LineDetector):
         #
         image = imageCameraPair.image
         # cv2.namedWindow('edges', cv2.WINDOW_NORMAL)
-        cv2.namedWindow('lines', cv2.WINDOW_NORMAL)
+        # cv2.namedWindow('lines', cv2.WINDOW_NORMAL)
 
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         edges = cv2.Canny(gray, 50, 150, apertureSize=7)
         # cv2.imshow('edges', edges)
 
         candidate_planes = []
-        lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
+        lines = cv2.HoughLines(edges, 1, np.pi / (16 * 180), 100)
         for rho, theta in lines[0]:
-            end_points = line_end_points_on_image(rho, theta, imageCameraPair.camera.resolution.to_tuple())
-            # a = np.cos(theta)
-            # b = np.sin(theta)
-            # x0 = a * rho
-            # y0 = b * rho
-            # x1 = int(x0 + 1000 * (-b))
-            # y1 = int(y0 + 1000 * (a))
-            # x2 = int(x0 - 1000 * (-b))
-            # y2 = int(y0 - 1000 * (a))
-            (x1, y1), (x2, y2) = end_points
+            x0 = np.cos(theta) * rho
+            y0 = np.sin(theta) * rho
+
+            if np.isclose(y0, 0.0):
+                # Handle the case with a vertical line:
+                x1, y1 = int(x0), 0
+                x2, y2 = int(x0), imageCameraPair.camera.resolution.height - 1
+                end_points = [(x1, y1), (x2, y2)]
+            else:
+                # The following function doesn't work for vertical lines
+                end_points = line_end_points_on_image(rho, theta, imageCameraPair.camera.resolution.to_tuple())
+                (x1, y1), (x2, y2) = end_points
+
             cv2.line(image, (x1, y1), (x2, y2), (0, 0, 255), 2)
             plane = self.get_plane_from_image_points(imageCameraPair.camera, end_points[0], end_points[1])
             candidate_planes.append(plane)
 
-        cv2.imshow('lines', image)
-        cv2.waitKey(0)
+        # cv2.imshow('lines', image)
+        # cv2.waitKey(0)
         return candidate_planes
 
     def get_plane_from_image_points(self, camera: Camera,
@@ -69,29 +72,28 @@ class HoughLineDetector(LineDetector):
         # If the cameras was looking at the position with azimuth of 0
         # and elevation 0, then it is looking exactly along the Y axis.
         # The azimuth is measured from the Y axis.
-        # direction = base_position.to_array() + np.array([0, 1, 0])
-        direction = np.array([0, 1, 0])
+        direction = np.array([0, 1, 0, 1])
 
         # Angles to radians
         azim_rads = azimuth / 180 * np.pi
         elev_rads = elevation / 180 * np.pi
 
-        rotation_matrix_azim = np.array([[np.cos(azim_rads), np.sin(azim_rads), 0],
-                                         [-np.sin(azim_rads), np.cos(azim_rads), 0],
-                                         [0, 0, 1]])
-        rotation_matrix_elev = np.array([[1, 0, 0],
-                                         [0, np.cos(elev_rads), -np.sin(elev_rads)],
-                                         [0, np.sin(elev_rads), np.cos(elev_rads)]])
-        rotation_matrix = np.dot(rotation_matrix_elev, rotation_matrix_azim)
-        oriented_direction = np.dot(rotation_matrix, direction)
+        translation = base_position.to_array()
+        transformation_matrix = get_transformation_matrix(-translation, azim_rads, elev_rads)
+        inverse_transformation_matrix = np.linalg.inv(transformation_matrix)
 
-        line = Line(base_position, Position.from_array(base_position.to_array() + oriented_direction))
+        oriented_direction = np.dot(inverse_transformation_matrix, direction)
+
+        line = Line(base_position, Position.from_array(oriented_direction))
         return line
+
 
 """
 The following functions originate from 
 https://arccoder.medium.com/process-the-output-of-cv2-houghlines-f43c7546deae
 """
+
+
 def line_end_points_on_image(rho: float, theta: float, image_shape: tuple):
     """
     Returns end points of the line on the end of the image

test_data_types.py
@@ -1,6 +1,8 @@
 from unittest import TestCase
+
 import numpy as np
-from data_types import CandidatePlane, Line, Position
+
+from data_types import Camera, CameraOrientation, CandidatePlane, ImageSize, Line, Position
 
 
 class TestCandidatePlane(TestCase):
@@ -15,3 +17,26 @@ class TestCandidatePlane(TestCase):
         coeffs = plane.get_coeffs()
 
         self.assertTrue(np.array_equal(coeffs, np.array([2, 2, 0, -6])))
+
+
+class TestCamera(TestCase):
+    def test_half_ifovs(self):
+        pixel_size = 3.45 * 1e-6
+        focal_length = 20 * 1e-3
+        elevation = 0
+        cam = Camera(Position(0, 0, 500),
+                     ImageSize(6480, 4860),
+                     CameraOrientation(0, elevation),
+                     focal_length, pixel_size)
+
+        half_width = int(cam.resolution.width / 2)
+        half_ifovs = cam.half_ifovs(num_pixels=half_width)
+        half_fov = np.sum(half_ifovs)
+
+        # The sum of angles should give half of FOV.
+        expected_half_fov = cam.fov_azim / 2
+        self.assertTrue(np.isclose(half_fov, expected_half_fov))
+
+        # Check the angles are decreasing
+        for i in range(1, len(half_ifovs)):
+            self.assertTrue(half_ifovs[i - 1] > half_ifovs[i])