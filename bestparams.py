3f55268d271a75f7f99bfcc179c2ab6db52f03f2
Ladislav Ondris <ladislav.ondris@seznam.cz>
2022-07-29 13:54:44 +0200

    Refactored bestparams search

--- bestparams.py
+++ bestparams.py
@@ -2,48 +2,84 @@
 This scripts runs evaluation for different parameters of the rotlinedet detection algorithm.
 Selects the parameters with the highest success rate on the testdataset.
 """
-import numpy as np
-import src.testdataset.utils as utils
 
+import argparse
+
+import numpy as np
+
+import src.testdataset.utils as utils
 from src.testdataset.algorithms.rotlinedet import get_detection_algorithm
 from src.testdataset.evaluation import Evaluator
 from src.testdataset.loading import SyntheticLaserDataset
 
-# Generate parameters
-filterSizes = np.array([10, 16, 20, 26, 30, 36, 40, 46, 50], dtype=float)
-slopeThreshold = np.array([0.025, 0.05, 0.075, 0.1, 0.15, 0.2], dtype=float)
-minPixelsThreshold = np.array([100, 200, 300, 400], dtype=float)
-params = utils.cartesian([filterSizes, slopeThreshold, minPixelsThreshold])
 
-# Run evaluation for each config
-evaluator = Evaluator()
-dataset = SyntheticLaserDataset('./dataset/testdataset')
+class BestParamsSeeker:
 
-success_counts = []
-stats = []
+    def __init__(self, exe_path: str, pixel_counts_path: str):
+        self.exe_path = exe_path
+        self.pixel_counts_path = pixel_counts_path
 
-for param_idx in range(params.shape[0]):
-    param = params[param_idx]
+        self.evaluator = Evaluator()
+        self.dataset = SyntheticLaserDataset('./dataset/testdataset')
 
-    detection_algorithm = get_detection_algorithm('../rotlinedet-gpu/cmake-build-release/src/rotlinedet_run',
-                                                  '../rotlinedet-gpu/src/scripts/columnPixelCounts.dat',
-                                                  filterSize=param[0], slopeThreshold=param[1], minPixelsThreshold=param[2])
+    def find_best_params(self):
+        params = self._generate_params()
+        self._run_evaluation_for_each_config(params)
+        self._select_and_print_best_params()
 
-    evaluator.evaluate(dataset, detection_algorithm, verbose=False)
-    # Save results in a container
-    success_counts.append(evaluator.success_count)
-    stats.append(evaluator.stats)
+    def _generate_params(self):
+        self.filterSizes = np.array([10, 16, 20, 26, 30, 36, 40, 46, 50], dtype=float)
+        self.slopeThreshold = np.array([0.025, 0.05, 0.075, 0.1, 0.15, 0.2], dtype=float)
+        self.minPixelsThreshold = np.array([100, 200, 300, 400], dtype=float)
+        params = utils.cartesian([self.filterSizes, self.slopeThreshold, self.minPixelsThreshold])
+        return params
 
-    print(f"{param_idx}: {evaluator.success_count}")
+    def _run_evaluation_for_each_config(self, params):
+        self._reset_stats()
 
-# Select and print the best params and results
-success_counts = np.array(success_counts)
-best_param_id = np.argmax(success_counts)
+        for param_idx in range(params.shape[0]):
+            param = params[param_idx]
+            self._evaluate_for_config(param)
+            self._save_results_for_later()
+            print(f"{param_idx}: {self.evaluator.success_count}")
 
-print("=================================================")
-print(f"Best params: --filterSize {filterSizes[best_param_id]} "
-      f"--slopeThreshold {slopeThreshold[best_param_id]} "
-      f"--minPixelsThreshold {minPixelsThreshold[best_param_id]}")
-evaluator.stats = stats[best_param_id]
-evaluator.print_stats()
-print("=================================================")
+    def _reset_stats(self):
+        self.success_counts = []
+        self.stats = []
+
+    def _evaluate_for_config(self, param):
+        detection_algorithm = get_detection_algorithm(self.exe_path, self.pixel_counts_path,
+                                                      filterSize=param[0], slopeThreshold=param[1],
+                                                      minPixelsThreshold=param[2])
+        self.evaluator.evaluate(self.dataset, detection_algorithm, verbose=False)
+
+    def _save_results_for_later(self):
+        self.success_counts.append(self.evaluator.success_count)
+        self.stats.append(self.evaluator.stats)
+
+    def _select_and_print_best_params(self):
+        success_counts = np.array(self.success_counts)
+        best_param_id = np.argmax(success_counts)
+
+        print("=================================================")
+        print(f"Best params: --filterSize {self.filterSizes[best_param_id]} "
+              f"--slopeThreshold {self.slopeThreshold[best_param_id]} "
+              f"--minPixelsThreshold {self.minPixelsThreshold[best_param_id]}")
+        self.evaluator.stats = self.stats[best_param_id]
+        self.evaluator.print_stats()
+        print("=================================================")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Find best parameters for rotlinedet detection algorithm')
+    parser.add_argument('--exe-path', action='store', type=str,
+                        default='../rotlinedet-gpu/cmake-build-release/src/rotlinedet_run')
+    parser.add_argument('--pixel-counts-path', action='store', type=str,
+                        default='../rotlinedet-gpu/src/scripts/columnPixelCounts.dat')
+    args = parser.parse_args()
+    return args
+
+
+args = parse_args()
+param_seeker = BestParamsSeeker(args.exe_path, args.pixel_counts_path)
+param_seeker.find_best_params()
